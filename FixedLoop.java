7c2ef05315be3eedd1472e620e500e0d33495fa7
BugHunter-X <askzenai@gmail.com>
2025-01-26 12:00:05 +0100

    Add FixedLoop.java file

---
+++ FixedLoop.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+            System.out.println(x);
+            x++; // Increment only once
+            if (x == 5) {
+                break; // Exit the loop when x is 5
+            }
+        }
+        System.out.println("Loop finished");
+    }
+}
